1ce72779abbaed37415b487268551bf2fbd74f5f
Rohin1212 <rohinchopra1212@gmail.com>
2020-10-01 13:36:07 +1000

    added hover on player search card

--- client/src/components/PlayerCard/PlayerCard.js
+++ client/src/components/PlayerCard/PlayerCard.js
@@ -2,10 +2,15 @@ import React from "react";
 import Avatar from "../Avatar";
 
 import Card from "../Card";
+import "./PlayerCard.css";
 const PlayerCard = ({ player }) => {
   return (
-    <Card bodyClassName="d-flex ">
-      <Avatar playerId={player.pid } playerName={player.name}  style={{ height: "50px", width: "50px" }}/>
+    <Card className="player-search-card" bodyClassName="d-flex ">
+      <Avatar
+        playerId={player.pid}
+        playerName={player.name}
+        style={{ height: "50px", width: "50px" }}
+      />
       <p className="align-self-center ml-1">{player.name}</p>
     </Card>
   );
